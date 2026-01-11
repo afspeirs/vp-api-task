1b18968f6c4d7ee993ffbe2e521897c4c246d116
Andrew Speirs <andrew@speirs.dev>
2026-01-11 21:11:27 +0000

    refactor: reduce Card component styles and markup

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,7 +8,7 @@ export function Card({
   product,
 }: CardProps) {
   return (
-    <li className="flex flex-col w-54 text-sm bg-white hover:shadow-lg transition-shadow overflow-hidden">
+    <li className="relative flex flex-col w-54 text-sm bg-white hover:shadow-lg transition-shadow overflow-hidden">
       <div className="w-full aspect-square overflow-hidden">
         <img
           className="object-contain size-full"
@@ -16,13 +16,11 @@ export function Card({
           alt={product.image?.attributes?.imageAltText || ''}
         />
       </div>
-      <div className="flex flex-col flex-1 gap-2 p-2">
+      <div className="p-2">
         <span className='line-clamp-3 text-md'>{product.productName}</span>
-        <div className="mt-auto">
-          <div className="inline-block bg-amber-400 px-2 py-1 rounded-2xl">
-            <span className="sr-only">Rated </span> {product.averageRating} <span className="sr-only"> out of 5</span>
-          </div>
-        </div>
+      </div>
+      <div className="absolute top-2 right-2 inline-block bg-amber-400 px-2 py-1 rounded-2xl">
+        <span className="sr-only">Rated </span> {product.averageRating?.toFixed(1)} <span className="sr-only"> out of 5</span>
       </div>
     </li>
   )
